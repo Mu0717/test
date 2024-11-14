d9f3698d9e76170f096f4debfb0f33b5306b2cc2
邓邵华 <441636706@qq.com>
2024-11-14 08:41:19 +0800

    修改App.vue中的msg为Jenkins

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App 123"/>
+    <HelloWorld msg="Jenkins"/>
   </div>
 </template>
 
